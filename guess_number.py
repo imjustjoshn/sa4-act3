c5ff41d41f0d0bf16e4d9408e9d80e9c64604297
Joshua Clark <joshuaclark2014@email.com>
2024-02-07 19:15:04 -0600

    Changed to allow users to continue guessing until right, or quit.

--- guess_number.py
+++ guess_number.py
@@ -3,8 +3,12 @@ number = 10
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
+while guess != "q":
+    if int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        guess = input("Sorry! Guess Again? (q to quit): ")
+
+if guess == "q":
     print(f"Sorry! The number was {number}.")
-    
