83695d5017b9b19ee33169d08427b8108ced4c9e
Joshua Clark <joshuaclark2014@email.com>
2024-02-07 19:22:38 -0600

    Added hints to push the guesser towards the target number.

--- guess_number.py
+++ guess_number.py
@@ -8,7 +8,11 @@ while guess != "q":
         print("Congratulations! You guessed the right number.")
         break
     else:
-        guess = input("Sorry! Guess Again? (q to quit): ")
+        if int(guess) > number:
+            print("Hmm, maybe a little lower?")
+        elif int(guess) < number:
+            print("Hmm, maybe a little higher?")
+        guess = input("Guess Again? (q to quit): ")
 
 if guess == "q":
     print(f"Sorry! The number was {number}.")
