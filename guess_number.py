1ce44bdacefe3ea936267e8ebb8130b9f8f6af1b
Joshua Clark <joshuaclark2014@email.com>
2024-02-07 19:34:54 -0600

    Added a limited number of guesses to get the right answer.

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,19 @@
 number = 10
+guesses_left = 5
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
 while guess != "q":
-    if int(guess) == number:
+    if guesses_left == 0:
+        print(f"Sorry! The number was {number}.")
+        break
+    elif int(guess) == number:
         print("Congratulations! You guessed the right number.")
         break
     else:
-        guess = input("Sorry! Guess Again? (q to quit): ")
+        guess = input(f"{guesses_left} Guesses Left, Guess Again? (q to quit): ")
+        guesses_left -= 1
 
 if guess == "q":
     print(f"Sorry! The number was {number}.")
